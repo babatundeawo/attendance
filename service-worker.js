3faf8f021315645faee51446317ef73cc8d405fc
Babátúndé Awóyẹmí <tunspiderman@gmail.com>
2026-09-23 17:51:53 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+var CACHE="attendance-cache-v3";
+var ASSETS=["./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
+  self.skipWaiting();
+});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch",function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).then(function(res){
+        var copy=res.clone();
+        caches.open(CACHE).then(function(c){c.put(e.request,copy);});
+        return res;
+      }).catch(function(){return cached;});
+    })
+  );
+});
